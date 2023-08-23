e69b5edc0456acdc2323f64096ce5ccb846f8068
erdemkose <erdem.kose@nordsec.com>
2023-08-24 02:16:52 +0300

    Remove static file serving

--- cmd/api.go
+++ cmd/api.go
@@ -22,11 +22,7 @@ func main() {
 	}
 
 	r := chi.NewRouter()
-	r.Get("/api/v1/resolvers/{resolver}/domains/{domain}", d.Show)
-	r.HandleFunc("/resolvers/{resolver}/domains/{domain}", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./static/index.html")
-	})
-	r.Handle("/*", http.FileServer(http.Dir("./static")))
+	r.Get("/v1/resolvers/{resolver}/domains/{domain}", d.Show)
 
 	l, err := net.Listen("unix", unixSocket)
 	if err != nil {
