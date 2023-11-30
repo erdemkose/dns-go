43fb7629b3dcf7e601771b48430631a631ac8b48
erdemkose <erdem.kose@nordsec.com>
2023-11-30 22:57:50 +0100

    Allow listening on a tcp port

--- cmd/api.go
+++ cmd/api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 
@@ -13,10 +14,6 @@ import (
 )
 
 func main() {
-	var unixSocket string
-	flag.StringVar(&unixSocket, "unix-socket", "dns-api.sock", "Unix socket")
-	flag.Parse()
-
 	d := domains.Controller{
 		DNS: dns.NewService(),
 	}
@@ -24,10 +21,17 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/v1/resolvers/{resolver}/domains/{domain}", d.Show)
 
-	l, err := net.Listen("unix", unixSocket)
+	network, address, ok := parseNetworkAndAddress()
+	if !ok {
+		log.Fatal("missing or invalid port and unix socket parameter")
+	}
+
+	l, err := net.Listen(network, address)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("Listening on %s://%s\n", network, address)
 
 	defer func(l net.Listener) {
 		err := l.Close()
@@ -38,4 +42,27 @@ func main() {
 
 	s := http.Server{Handler: r}
 	log.Fatal(s.Serve(l))
+}
+
+func parseNetworkAndAddress() (string, string, bool) {
+	var port, unixSocket string
+	flag.StringVar(&port, "port", "", "TCP Port")
+	flag.StringVar(&unixSocket, "unix-socket", "", "Unix socket")
+	flag.Parse()
+
+	if port == "" {
+		if p, found := os.LookupEnv("PORT"); found {
+			port = p
+		}
+	}
+
+	if port != "" {
+		return "tcp", "0.0.0.0:" + port, true
+	}
+
+	if unixSocket != "" {
+		return "unix", unixSocket, true
+	}
+
+	return "", "", false
 }
