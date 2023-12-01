b3bd8d7bed2a538c48a0abdf668f4f12c11a4584
erdemkose <erdem.kose@nordsec.com>
2023-12-01 16:45:04 +0100

    Log DNS resolve requests

--- internal/api/domains/handler.go
+++ internal/api/domains/handler.go
@@ -2,6 +2,7 @@ package domains
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -19,11 +20,15 @@ func (c *Controller) Show(rw http.ResponseWriter, r *http.Request) {
 
 	records, err := c.DNS.Resolve(r.Context(), domain, resolver)
 	if err != nil {
+		log.Printf("DNS resolve failure: domain: %s, resolver:%s, err:%s\n", domain, resolver, err.Error())
+
 		rw.Header().Set("Content-Type", "application/json")
 		_, _ = rw.Write([]byte(err.Error()))
 
 		return
 	}
+
+	log.Printf("DNS resolve success: domain: %s, resolver:%s\n", domain, resolver)
 
 	out, _ := json.Marshal(records)
 
